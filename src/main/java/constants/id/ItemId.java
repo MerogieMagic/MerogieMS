9591d24ab5f76d71d11b83160c45bf9604db8690
Slimy <slimycs96@gmail.com>
2025-06-25 06:18:16 +0800

    updated isChair function for other players to view chairs

--- src/main/java/constants/id/ItemId.java
+++ src/main/java/constants/id/ItemId.java
@@ -68,8 +68,8 @@ public class ItemId {
     private static final int CHAIR_MAX = FISHING_CHAIR;
 
     public static boolean isChair(int itemId) {
-        return itemId >= CHAIR_MIN && itemId <= CHAIR_MAX;
-        // alt: return itemId / 10000 == 301;
+//        return itemId >= CHAIR_MIN && itemId <= CHAIR_MAX;
+        return itemId / 10000 == 301; // for higher version chairs
     }
 
     // Throwing star
